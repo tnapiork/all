1d3bca08f5653f3de41fc2b43fc2e2da436baa4c
Tomasz Napiórkowski <spin86@tlen.pl>
2014-02-23 11:17:47 +0100

    komentarz

---
+++ workspace_v5_5/lekcja00/Debug/blink.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: /opt/ti/ccsv5/ccs_base/msp430/include/msp430.h
+blink.obj: /opt/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h
+blink.obj: /opt/ti/ccsv5/ccs_base/msp430/include/in430.h
+blink.obj: /opt/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../blink.c: 
+/opt/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+/opt/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h: 
+/opt/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+/opt/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
